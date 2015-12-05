b0cdd678d09703f5d254f70770ae6a11672c6670
Loïc Hamet <loic.hamet@gmail.com>
2015-12-06 00:32:30 +0100

    template string & node

--- assets/src/js/modules/template.js
+++ assets/src/js/modules/template.js
@@ -1,5 +1,25 @@
 import templates from '../../../../tmp/templates';
 
-export default function template (key, data) {
+function string (key, data) {
 	return templates[key](data);
+}
+
+function node (key, data) {
+	let str = string(key, data);
+	let frag = document.createDocumentFragment();
+	let div = document.createElement('div');
+	console.log(str);
+	div.innerHTML = str;
+	console.log(div);
+	while (div.firstChild) {
+		console.log(div.firstChild);
+		frag.appendChild(div.firstChild);
+	}
+	console.log(frag);
+	return frag;
+}
+
+export default {
+	string: string,
+	node: node
 }
